7aa493560941652ccfdbfc83bc02b2458de1dfc3
jaehongkim1028 <fnapdlwl@naver.com>
2022-11-26 22:40:32 +0900

    flask upload implement

--- server.py
+++ server.py
@@ -1,10 +1,59 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
 # render_templete은 .html 파일을 text형태로 변환
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static/uploads/'
+ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg', 'gif'])
+
+app.secret_key = "secret_key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+def allowed_file(filename): 
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# 루트 라우팅
 @app.route('/')
 def index():
     return render_template('home.html')
-    # templates 폴더를 만들고 그 안에 html파일을 넣어야함. 그러나 css는 적용되지않음.
+     # templates 폴더를 만들고 그 안에 html파일을 넣어야함. 그러나 css는 적용되지않음.
+
+@app.route('/',methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #print('upload_image filename:' + filename)
+        flash('Image successfully uploaded and displayed below')
+        return render_template('home.html', filename=filename)
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+# 파일 업로드 처리 라우팅
+
+# @app.route('/upload/', methods = ['POST'])
+# def upload_file():
+#     if request.method == 'POST':
+#         f = request.files['file']
+#         if f and allowed_file(f.filename):
+#             # 저장할 경로 + 파일명
+#         filename = secure_filename(f.filename)
+#         f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#         return redirect(url_for('uploaded_file', filename=filename))
+
+
+
+# 서버실행
 app.run(debug=True)
+
---
+++ testserver.py
@@ -0,0 +1,62 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+topics = [
+    {'id': 1, 'title': 'html', 'body': 'html is ...'},
+    {'id': 2, 'title': 'css', 'body': 'css is ...'},
+    {'id': 3, 'title': 'javascript', 'body': 'javascript is ...'} 
+]
+
+@app.route('/')
+def index():
+    liTags = ''
+    for topic in topics:
+        liTags = liTags + f'<li><a href="/read/{topic["id"]}/">{topic["title"]}</a></li>'
+    return f''' <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <h1><a href="/">WEB</a></h1>
+            <ol>
+                {liTags}
+            </ol>
+            <h2>Welcome</h2>
+            Hello, Web
+        </body>
+    </html>'''  
+@app.route('/create/')
+def create():
+    return 'Create'
+
+@app.route('/read/<int:num>/')
+def read(num):
+    title = ''
+    body = ''
+    for topic in topics:
+        if num == topic['id']:
+            title = topic['title']
+            body = topic['body']
+    return f''' <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <h1><a href="/">{title}</a></h1>
+            <h2>{body}</h2>
+            
+        </body>
+    </html>'''  
+
+
+
+app.run(debug=True)
